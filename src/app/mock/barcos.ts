9bbcc1943ed423e7eb7e29bb863e4453c4a6b42a
JAVIER\menaj <menajavier2002@gmail.com>
2022-10-25 16:58:41 +0200

    TERMINADO FALTA  AÑADIR ESTILOS SI DA TIEMPO MAÑANA

--- src/app/mock/barcos.ts
+++ src/app/mock/barcos.ts
@@ -1,9 +1,9 @@
 import { Barcos } from "../interface/interfazBarco";
 
 export const barcos: Barcos[] = [
-  {id: 1, nombre: "Santa maria", tipo: "FFG", imagen: ""},
-  {id: 2, nombre: "Numancia", tipo: "FFG", imagen: ""},
-  {id: 3, nombre: "Reina Sofia", tipo: "FFG", imagen: ""},
-  {id: 4, nombre: "Navarra", tipo: "FFG", imagen: ""},
-  {id: 5, nombre: "Cristobal Colon", tipo: "FFG", imagen: ""},
+  {id: 1, nombre: "Santa maria", tipo: "Carguero", imagen: "../../assets/images/sm.jpg"},
+  {id: 2, nombre: "Numancia", tipo: "Velero", imagen: "../../assets/images/Numancia.jpg"},
+  {id: 3, nombre: "Reina Sofia", tipo: "Fragata", imagen: "../../assets/images/rs.webp"},
+  {id: 4, nombre: "Navarra", tipo: "Lancha", imagen: "../../assets/images/navarra.jpg"},
+  {id: 5, nombre: "Cristobal Colon", tipo: "FFG", imagen: "../../assets/images/cl.jpg"},
 ]
